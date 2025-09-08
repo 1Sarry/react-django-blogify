bf0f8e41507cd52806df91ac58f3a8cff153cb95
saronmeles4 <saronmeles4@gmail.com>
2025-09-08 20:27:08 +0300

    Connect django  backend with Mysql server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'bloguser',
+        'PASSWORD': 'YourStrongPassword123!',
+        'HOST': '127.0.0.1',  # or '127.0.0.1'
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
